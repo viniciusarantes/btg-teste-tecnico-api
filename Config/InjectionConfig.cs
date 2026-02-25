ba86c871fa30f8a46d9d886b2d5d033a507c7942
Vinicius Paiva <vinicius.arantespaiva@gmail.com>
2026-02-24 22:34:40 -0300

    add injection config

---
+++ Config/InjectionConfig.cs
@@ -0,0 +1,24 @@
+﻿using TesteTecnicoBTG.Data;
+using TesteTecnicoBTG.Data.Interfaces;
+using TesteTecnicoBTG.Data.Repositories;
+using TesteTecnicoBTG.Services;
+using TesteTecnicoBTG.Services.Interfaces;
+
+namespace TesteTecnicoBTG.Config
+{
+    public static class InjectionConfig
+    {
+        public static IServiceCollection AddDependencyInjection(this IServiceCollection services)
+        {
+            // Database
+            services.AddSingleton<DBConnectionFactory>();
+
+            // Services
+            services.AddScoped<IUsuarioService, UsuarioService>();
+
+            // Repositories
+            services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+            return services;
+        }
+    }
+}
